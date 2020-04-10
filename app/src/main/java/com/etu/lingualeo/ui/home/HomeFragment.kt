3fef929203723e6b0bcd62e768e1fa616e4b0b4d
Куценок Леонид <l.kutsenok@robotbull.com>
2020-04-11 02:08:28 +0300

    Верстка списка слов на главном экране

--- app/src/main/java/com/etu/lingualeo/ui/home/HomeFragment.kt
+++ app/src/main/java/com/etu/lingualeo/ui/home/HomeFragment.kt
@@ -4,28 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.etu.lingualeo.R
+import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView
+import kotlin.collections.ArrayList
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    var words = ArrayList<WordListItem>()
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val recyclerView: FastScrollRecyclerView = root.findViewById(R.id.recycler)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        getMockWords()
+
+        recyclerView.adapter = WordListAdapter(words)
         return root
+    }
+
+    fun getMockWords() {
+        words.add(WordListItem("indifferent", "безразличный", "https://contentcdn.lingualeo.com/uploads/picture/3053472.png"))
+        words.add(WordListItem("help", "помощь"))
+        words.add(WordListItem("accomplish", "достигать"))
+        words.add(WordListItem("acknowledge", "признавать"))
+        words.add(WordListItem("acquaintance", "знакомый"))
+        words.add(WordListItem("agenda", "повестка дня"))
+        words.add(WordListItem("biased", "предвзятый"))
+        words = ArrayList(words.map { word ->
+            word.word = word.word.capitalize()
+            word.translation = word.translation.capitalize()
+            word
+        })
+        words = ArrayList(words.sortedBy { it.word })
     }
 }
---
+++ app/src/main/java/com/etu/lingualeo/ui/home/WordListAdapter.kt
@@ -0,0 +1,52 @@
+package com.etu.lingualeo.ui.home
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.etu.lingualeo.R
+import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView.SectionedAdapter
+import com.squareup.picasso.Picasso
+
+class WordListAdapter(val items: ArrayList<WordListItem>) :
+    RecyclerView.Adapter<WordListAdapter.ViewHolder>(), SectionedAdapter {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(viewType, parent, false))
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return R.layout.word_list_item
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items.get(position)
+        holder.word.setText(item.word)
+        holder.translation.setText(item.translation)
+        if(item.imageUrl != null) Picasso.get().load(item.imageUrl).into(holder.image)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun getSectionName(position: Int): String {
+        return items.get(position).word.toCharArray().first().toString()
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var word: TextView
+        var translation: TextView
+        var image: ImageView
+
+        init {
+            word = itemView.findViewById(R.id.primary_text)
+            translation = itemView.findViewById(R.id.sub_text)
+            image = itemView.findViewById(R.id.media_image)
+        }
+    }
+}
+
+class WordListItem(var word: String, var translation: String, var imageUrl: String? = null)
