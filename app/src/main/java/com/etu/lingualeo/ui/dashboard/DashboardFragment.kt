70fe3b18c3f748dd3f063d50c3e2e13eaa660536
Куценок Леонид <l.kutsenok@robotbull.com>
2020-04-13 02:01:16 +0300

    Возможность выбора разных источников изображения

--- app/src/main/java/com/etu/lingualeo/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/etu/lingualeo/ui/dashboard/DashboardFragment.kt
@@ -1,6 +1,5 @@
 package com.etu.lingualeo.ui.dashboard
 
-import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -9,30 +8,20 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.provider.MediaStore
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import com.etu.lingualeo.R
 import com.etu.lingualeo.RandomString
 import com.etu.lingualeo.textEditor.TextEditorActivity
-import com.etu.lingualeo.wordSelector.WordSelectorActivity
 import com.google.android.gms.vision.Frame
-import com.google.android.gms.vision.text.TextBlock
 import com.google.android.gms.vision.text.TextRecognizer
 import java.io.File
-import java.text.DateFormat
-import java.text.SimpleDateFormat
-import java.util.*
-import java.util.concurrent.ThreadLocalRandom
 
 
 const val PERMISSION_CAMERA = 1
-const val PERMISSION_GALLERY = 2
 
 const val RESULT_CAMERA = 1
 
@@ -46,22 +35,8 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-
-        val status = checkSelfPermission(context!!, Manifest.permission.CAMERA)
-        if (status == PackageManager.PERMISSION_GRANTED) {
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            fileName = prepareFile()
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileName)
-            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-            startActivityForResult(intent, RESULT_CAMERA)
-        } else {
-            requestPermissions(arrayOf(Manifest.permission.CAMERA), PERMISSION_CAMERA)
-        }
-
-//        root.findViewById<Button>(R.id.button).setOnClickListener {
-//            startActivity(Intent(activity, WordSelectorActivity::class.java))
-//}
-
+        registerForContextMenu(root)
+        getPicture()
         return root
     }
 
@@ -73,13 +48,26 @@ class DashboardFragment : Fragment() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             PERMISSION_CAMERA -> if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                fileName = prepareFile()
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileName)
-                startActivityForResult(intent, RESULT_CAMERA)
+                getPicture()
             }
         }
+    }
+
+    fun getPicture() {
+        val intentGallery = Intent(Intent.ACTION_GET_CONTENT);
+        intentGallery.addCategory(Intent.CATEGORY_OPENABLE);
+        intentGallery.setType("image/*")
+
+        val intentCamera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        fileName = prepareFile()
+        intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, fileName)
+        intentCamera.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+
+        val chooser = Intent(Intent.ACTION_CHOOSER)
+        chooser.putExtra(Intent.EXTRA_INTENT, intentGallery)
+        chooser.putExtra(Intent.EXTRA_TITLE, "Выбрать источник")
+        chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(intentCamera))
+        startActivityForResult(chooser, RESULT_CAMERA)
     }
 
     fun prepareFile(): Uri {
@@ -89,24 +77,22 @@ class DashboardFragment : Fragment() {
             context!!.getApplicationContext().getPackageName() + ".p_provider",
             File.createTempFile(RandomString(12).nextString(), ".jpg", outputDir)
         );
-        Log.i("file", outputFile.toString())
         return outputFile
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == RESULT_CAMERA && resultCode == Activity.RESULT_OK) {
-//            Handler().postDelayed({
-                val picture = MediaStore.Images.Media.getBitmap(context?.contentResolver, fileName);
-                val text = getTextFromImage(picture)
-                val intent = Intent(activity, TextEditorActivity::class.java)
-                intent.putExtra("text", text)
-                startActivity(intent)
-                val a: View = activity!!.findViewById(R.id.navigation_home)
-                Handler().postDelayed({
-                    a.performClick()
-                }, 100)
-//            }, 100)
+            if (data?.data != null) fileName = data.data!!
+            val picture = MediaStore.Images.Media.getBitmap(context?.contentResolver, fileName);
+            val text = getTextFromImage(picture)
+            val intent = Intent(activity, TextEditorActivity::class.java)
+            intent.putExtra("text", text)
+            startActivity(intent)
+            val a: View = activity!!.findViewById(R.id.navigation_home)
+            Handler().postDelayed({
+                a.performClick()
+            }, 100)
         }
     }
 
@@ -119,7 +105,6 @@ class DashboardFragment : Fragment() {
             sb.append(items.valueAt(i).value)
             sb.append("\n")
         }
-        Log.i("aa", sb.toString())
         return sb.toString()
     }
 }
