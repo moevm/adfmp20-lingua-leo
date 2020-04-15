c9eb97560968da79ade768c9ac2b0ff95a2b40b1
Куценок Леонид <l.kutsenok@robotbull.com>
2020-04-15 18:03:40 +0300

    Fix

--- app/src/main/java/com/etu/lingualeo/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/etu/lingualeo/ui/dashboard/DashboardFragment.kt
@@ -1,5 +1,6 @@
 package com.etu.lingualeo.ui.dashboard
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -8,9 +9,9 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.provider.MediaStore
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import com.etu.lingualeo.R
@@ -18,6 +19,7 @@ import com.etu.lingualeo.RandomString
 import com.etu.lingualeo.textEditor.TextEditorActivity
 import com.google.android.gms.vision.Frame
 import com.google.android.gms.vision.text.TextRecognizer
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 import java.io.File
 
 
@@ -35,9 +37,23 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        registerForContextMenu(root)
-        getPicture()
+        Handler().postDelayed({
+            activity!!.runOnUiThread {
+                registerForContextMenu(button)
+                button.performLongClick()
+            }
+        }, 30)
         return root
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        super.onContextItemSelected(item)
+        when(item.toString()) {
+            "Камера" -> checkCameraPermissionsAndRun()
+            "Галерея" -> startGalleryActivity()
+            "Текст" -> startEditTextActivity(null)
+        }
+        return true
     }
 
     override fun onRequestPermissionsResult(
@@ -53,21 +69,23 @@ class DashboardFragment : Fragment() {
         }
     }
 
-    fun getPicture() {
-        val intentGallery = Intent(Intent.ACTION_GET_CONTENT);
-        intentGallery.addCategory(Intent.CATEGORY_OPENABLE);
-        intentGallery.setType("image/*")
+    override fun onCreateContextMenu(
+        menu: ContextMenu,
+        v: View,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menu.add(0, v.id, 0, "Камера")
+        menu.add(0, v.id, 0, "Галерея")
+        menu.add(0, v.id, 0, "Текст")
+    }
 
+    fun getPicture() {
         val intentCamera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         fileName = prepareFile()
         intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, fileName)
         intentCamera.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-
-        val chooser = Intent(Intent.ACTION_CHOOSER)
-        chooser.putExtra(Intent.EXTRA_INTENT, intentGallery)
-        chooser.putExtra(Intent.EXTRA_TITLE, "Выбрать источник")
-        chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(intentCamera))
-        startActivityForResult(chooser, RESULT_CAMERA)
+        startActivityForResult(intentCamera, RESULT_CAMERA)
     }
 
     fun prepareFile(): Uri {
@@ -86,13 +104,7 @@ class DashboardFragment : Fragment() {
             if (data?.data != null) fileName = data.data!!
             val picture = MediaStore.Images.Media.getBitmap(context?.contentResolver, fileName);
             val text = getTextFromImage(picture)
-            val intent = Intent(activity, TextEditorActivity::class.java)
-            intent.putExtra("text", text)
-            startActivity(intent)
-            val a: View = activity!!.findViewById(R.id.navigation_home)
-            Handler().postDelayed({
-                a.performClick()
-            }, 100)
+            startEditTextActivity(text)
         }
     }
 
@@ -106,5 +118,31 @@ class DashboardFragment : Fragment() {
             sb.append("\n")
         }
         return sb.toString()
+    }
+
+    fun startGalleryActivity() {
+        val intentGallery = Intent(Intent.ACTION_GET_CONTENT);
+        intentGallery.addCategory(Intent.CATEGORY_OPENABLE);
+        intentGallery.setType("image/*")
+        startActivityForResult(intentGallery, RESULT_CAMERA)
+    }
+
+    fun startEditTextActivity(text: String?) {
+        val intent = Intent(activity, TextEditorActivity::class.java)
+        if(text != null) intent.putExtra("text", text)
+        startActivity(intent)
+        val a: View = activity!!.findViewById(R.id.navigation_home)
+        Handler().postDelayed({
+            a.performClick()
+        }, 100)
+    }
+
+    fun checkCameraPermissionsAndRun() {
+        val status = checkSelfPermission(context!!, Manifest.permission.CAMERA)
+        if(status == PackageManager.PERMISSION_GRANTED) {
+            getPicture()
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), PERMISSION_CAMERA)
+        }
     }
 }
